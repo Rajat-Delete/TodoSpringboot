2ce71837395b8a9cee0254c3dfb922142e40bc73
Rajat Sharma <rajatxd007@gmail.com>
2024-10-22 16:05:42 +0530

    Added post api to todos

--- src/main/java/com/example/TodoAppSpring/TodoController.java
+++ src/main/java/com/example/TodoAppSpring/TodoController.java
@@ -1,6 +1,9 @@
 package com.example.TodoAppSpring;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -16,6 +19,19 @@ public class TodoController {
         todos.add(new Todo(1, false, "Todo 1", 1));
         todos.add(new Todo(2, true, "Todo 2", 2));
 
+    }
+
+
+    @GetMapping("/todos")
+    public List<Todo> getTodos(){
+        return todos;
+    }
+
+
+    @PostMapping("/todos")
+    public Todo createTodo(@RequestBody Todo todo){
+        todos.add(todo);
+        return todo;
     }
 
 }
