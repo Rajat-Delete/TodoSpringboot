9b1cf7c4e5864351f84fd11c634338a1f31b4710
Rajat Sharma <rajatxd007@gmail.com>
2024-10-22 18:16:47 +0530

    Added code for Response Structure and versioning

--- src/main/java/com/example/TodoAppSpring/TodoController.java
+++ src/main/java/com/example/TodoAppSpring/TodoController.java
@@ -2,7 +2,6 @@ package com.example.TodoAppSpring;
 
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/todos")
 public class TodoController {
 
     public static List<Todo> todos;
@@ -23,20 +23,30 @@ public class TodoController {
     }
 
 
-    @GetMapping("/todos")
+    @GetMapping("")
     public ResponseEntity<List<Todo>> getTodos(){
         return ResponseEntity.status(HttpStatus.OK).body(todos);
     }
 
 
-    @PostMapping("/todos")
-
+    @PostMapping("")
     public ResponseEntity<Todo> createTodo(@RequestBody Todo todo){
         /*
         We can either use this annotation to set the response code @ResponseStatus(HttpStatus.CREATED) or use ResponseEntity class
          */
         todos.add(todo);
         return ResponseEntity.status(HttpStatus.CREATED).body(todo);
+    }
+
+
+    @GetMapping("/{todoId}")
+    public ResponseEntity<Todo> getTodoById(@PathVariable(value = "todoId") long todoId){
+        for(Todo t : todos){
+            if(t.getId() == todoId){
+                return ResponseEntity.ok(t);
+            }
+        }
+        return ResponseEntity.notFound().build();
     }
 
 }
